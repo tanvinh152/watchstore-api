f9c44ff604aadb59cdd31375d1721d4afba41c10
tanvinh152 <54593144+tanvinh152@users.noreply.github.com>
2022-12-07 12:06:28 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,18 +1,24 @@
 module.exports = ({ env }) => ({
-  connection: {
-    client: "mysql",
-    connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "store"),
-      user: env("DATABASE_USERNAME", "root"),
-      password: env("DATABASE_PASSWORD", "150299"),
-      ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "mysql",
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 3306),
+        database: env("DATABASE_NAME", "store"),
+        user: env("DATABASE_USERNAME", "root"),
+        password: env("DATABASE_PASSWORD", "150299"),
+        //add this line
+        ssl: {
+          rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+        },
       },
-    },
-    options: {
-      ssl: env.bool("DATABASE_SSL", false),
+      // add this line
+      options: {
+        ssl: env.bool("DATABASE_SSL", false),
+      },
     },
   },
 });
